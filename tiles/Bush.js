d6cafb1c72d0e569a28d4d8f0c28a8f19b7a54b7
Damon Achey <Damon@Achey.net>
2025-10-15 12:38:39 -0600

    Improve bush tile

--- tiles/Bush.js
+++ tiles/Bush.js
@@ -18,24 +18,43 @@ Bush.prototype.render = function(ctx, canvas) {
     ctx.lineWidth = 2;
     ctx.lineCap = 'round';
     
-    // Draw bush as simple curved lines (stick figure style)
+    // Draw bush as smooth cloud outline with 5 lobes (taller and better centered)
     var centerX = screenX + 32;
-    var baseY = screenY + 64;
+    var centerY = screenY + 32;
     
-    // Main bush outline (curved bumpy shape)
+    // Draw cloud-like bush with 5 connected lobes
     ctx.beginPath();
-    ctx.moveTo(screenX + 16, baseY - 16);
-    ctx.quadraticCurveTo(screenX + 8, baseY - 32, screenX + 20, baseY - 40);
-    ctx.quadraticCurveTo(centerX, baseY - 48, screenX + 44, baseY - 40);
-    ctx.quadraticCurveTo(screenX + 56, baseY - 32, screenX + 48, baseY - 16);
-    ctx.stroke();
     
-    // Add a few simple leaf details
-    ctx.beginPath();
-    ctx.moveTo(screenX + 24, baseY - 24);
-    ctx.lineTo(screenX + 28, baseY - 28);
-    ctx.moveTo(screenX + 36, baseY - 30);
-    ctx.lineTo(screenX + 40, baseY - 34);
+    // Start from bottom left
+    ctx.moveTo(screenX + 16, centerY + 19);
+    
+    // Far left lobe
+    ctx.quadraticCurveTo(screenX + 8, centerY + 3, screenX + 16, centerY - 5);
+    
+    // Left to center-left transition
+    ctx.quadraticCurveTo(screenX + 20, centerY - 11, screenX + 24, centerY - 9);
+    
+    // Center-left lobe
+    ctx.quadraticCurveTo(screenX + 22, centerY - 17, screenX + 28, centerY - 15);
+    
+    // Center-left to center transition
+    ctx.quadraticCurveTo(screenX + 30, centerY - 21, centerX, centerY - 19);
+    
+    // Center to center-right transition
+    ctx.quadraticCurveTo(screenX + 34, centerY - 21, screenX + 36, centerY - 15);
+    
+    // Center-right lobe
+    ctx.quadraticCurveTo(screenX + 42, centerY - 17, screenX + 40, centerY - 9);
+    
+    // Center-right to right transition
+    ctx.quadraticCurveTo(screenX + 44, centerY - 11, screenX + 48, centerY - 5);
+    
+    // Far right lobe
+    ctx.quadraticCurveTo(screenX + 56, centerY + 3, screenX + 48, centerY + 19);
+    
+    // Bottom connection
+    ctx.lineTo(screenX + 16, centerY + 19);
+    
     ctx.stroke();
     
     // Draw bounding box if enabled
