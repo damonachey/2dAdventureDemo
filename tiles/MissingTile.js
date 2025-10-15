7094e1fc30e1f06a11998183406e1cf8b9d1fbe5
Damon Achey <Damon@Achey.net>
2025-10-15 12:26:27 -0600

    Add missing tile

---
+++ tiles/MissingTile.js
@@ -0,0 +1,44 @@
+function MissingTile(x, y, originalName) {
+    this.x = x;
+    this.y = y;
+    this.width = 64;
+    this.height = 64;
+    this.originalName = originalName || 'Unknown';
+}
+
+MissingTile.prototype.update = function(deltaTime) {
+    // No update logic needed for missing tile
+};
+
+MissingTile.prototype.render = function(ctx, canvas) {
+    // Convert world coordinates to screen coordinates
+    var screenX = (canvas.width / 2) - state.player.x + this.x;
+    var screenY = (canvas.height / 2) + state.player.y - this.y - this.height;
+
+    // Draw a large red X to indicate missing tile
+    ctx.strokeStyle = 'red';
+    ctx.lineWidth = 4;
+    ctx.lineCap = 'round';
+
+    // Draw X from corner to corner
+    ctx.beginPath();
+    // Top-left to bottom-right
+    ctx.moveTo(screenX + 8, screenY + 8);
+    ctx.lineTo(screenX + 56, screenY + 56);
+    // Top-right to bottom-left
+    ctx.moveTo(screenX + 56, screenY + 8);
+    ctx.lineTo(screenX + 8, screenY + 56);
+    ctx.stroke();
+
+    // Draw border around tile
+    ctx.strokeStyle = 'darkRed';
+    ctx.lineWidth = 2;
+    ctx.strokeRect(screenX, screenY, this.width, this.height);
+
+    // Draw bounding box if enabled
+    if (showBoundingBoxes) {
+        ctx.strokeStyle = 'red';
+        ctx.lineWidth = 1;
+        ctx.strokeRect(screenX, screenY, this.width, this.height);
+    }
+};
